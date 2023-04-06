4fa55dd15873cea9fc7537b55616174333d20fc5
adilummar <madilummer@gmail.com>
2023-04-06 17:12:08 +0530

    setting up

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
 //using selectors inside the element
 // traversing the dom
+const plusBtn = document.querySelectorAll('.plus-icon')
+const minusBtn = document.querySelectorAll('.minus-icon')
+const question = document.querySelectorAll('.question')
+
+plusBtn.forEach(item=>{
+    item.addEventListener('click',(e)=>{
+        let style = e.currentTarget.parentNode.parentNode.parentNode
+        style.classList.add('show-text')
+        console.log(style);
+    })
+})
+
+minusBtn.forEach(item=>{
+    item.addEventListener('click',e=>{
+        let style = e.currentTarget.parentNode.parentNode.parentNode
+        style.classList.remove('show-text')
+    })
+})
